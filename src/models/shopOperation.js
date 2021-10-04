ff577b7685117fffe832d8fca1798b767ecfdd49
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-04 01:57:38 -0500

    se modifica el modelo y las routes

---
+++ src/models/shopOperation.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const registerUser = new Schema({
+    name:{type: String, required: true},
+    email:{type: String, required: true},
+    country:{type: String, required: true},
+    city:{type: String, required: true},
+    password:{type: String, required: true},
+    roul:{type: String, required: true},
+    nameShop:{type: String, required: true}
+});
+
+module.exports = mongoose.model('user', registerUser);
--- src/routes/shoproutes.js
+++ src/routes/shoproutes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status:'API shop!'
-    });
-});  
+const userRegister = require('../models/shopOperation')
+
+router.get('/', async (req, res) => {
+    const register = await userRegister.find();
+        res.json(register);
+    });    
+
+router.post('/', async (req, res) => {
+    const {name, email, country, city, password, roul, nameShop} = req.body;
+    const register = new userRegister({name, email, country, city, password, roul, nameShop});
+    await register.save();
+    res.json({status:'register OK!!'});
+    });   
+  
+
+
 
 module.exports = router;
