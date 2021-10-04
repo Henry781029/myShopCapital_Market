82770246148e6358f590bbc4967beff8817cda4f
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-04 15:49:07 -0500

    se crean en routes el buscar y eliminar

--- src/routes/shoproutes.js
+++ src/routes/shoproutes.js
@@ -6,7 +6,12 @@ const userRegister = require('../models/shopOperation')
 router.get('/', async (req, res) => {
     const register = await userRegister.find();
         res.json(register);
-    });    
+    });  
+    
+router.get('/:id', async (req, res)=>{
+    const SearchUser = await userRegister.findById(req.params.id);
+    res.json(SearchUser);
+});
 
 router.post('/', async (req, res) => {
     const {name, email, country, city, password, roul, nameShop} = req.body;
@@ -19,8 +24,13 @@ router.put('/:id', async(req, res) => {
     const {name, email, country, city, password, roul, nameShop} = req.body; 
     const UpdateuserRegister = {name, email, country, city, password, roul, nameShop};
     await userRegister.findByIdAndUpdate(req.params.id, UpdateuserRegister);
-    res.json({status:'User Udated'})
+    res.json({status:'User Udated'});
 });
 
+router.delete('/:id', async(req, res) => {
+    await userRegister.findByIdAndRemove(req.params.id, UpdateuserRegister);
+    res.json({status:'User Delete'})
+
+});
 
 module.exports = router;
